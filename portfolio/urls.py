65bc0f43c23ea6202d6b9aae04f3939b7742cdac
Varun Garg <v8garg@uwaterloo.ca>
2020-04-18 18:50:32 -0400

    Created a new path to view an individual job

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.varun, name='varun'),
+    path('', jobs.views.homepage, name='home'),
+    path('jobs/<int:job_id>', jobs.views.detail, name='detail'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
